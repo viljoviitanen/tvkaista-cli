6299b6145192a2947152fe5854dafdd09c3dfdc6
Matti Pöllä <mpo@iki.fi>
2009-10-22 21:31:18 +0000

    Version 0.53, implemented play mode (view in browser).

--- setup.py
+++ setup.py
@@ -9,16 +9,14 @@ reload(sys).setdefaultencoding("UTF-8")
 
 setup(name='tvkaista-cli',
       description='Command line interface to tvkaista.fi PVR service',
-      version='0.52',
+      version='0.53',
       packages = find_packages(),
-      install_requires = ['httplib2'],
+      #install_requires = ['httplib2>0.3'], # Results in an 404 error (2009-10-22)
       scripts=['scripts/tvkaista'],
-      modules=['modules/tvkaista_alpha.py'],
       author="Matti Pöllä",
       author_email="mpo@iki.fi",
       url="http://code.google.com/p/tvkaista-cli/",
       license='GPL',
       keywords='tvkaista pvr video',
-      long_description='TVkaista.fi is a PVR (personal video recorder) service with a www interface. This script allows registered tvkaista users to search, download and stream programs from the command line.'
-      )
+      long_description='TVkaista.fi is a PVR (personal video recorder) service with a www interface. This script allows registered tvkaista users to search, download and stream programs from the command line.')
 
